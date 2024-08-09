2a3efad756476420432b7a9e5c74ed3786c0537d
LAI NHAT DUONG PHAN <greaves-forced-01@icloud.com>
2024-08-09 16:42:04 -0600

    Add files via upload

---
+++ asn-js-PROVIDED 2/scripts/contact.js
@@ -0,0 +1,13 @@
+// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+
+// hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
+
+document.addEventListener("DOMContentLoaded", function() {
+    var form = document.querySelector("form");
+    var mainContent = document.querySelector(".contact");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        mainContent.innerHTML = "<h2>Thank you for your message!</h2>";
+    });
+});
